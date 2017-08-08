cc3567162c1fd9363e14534259876da543c1eb3c
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-08-08 17:16:06 +0900

    adjust js

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,26 @@
-// $(function() {
-//     $('.pre-signUp').click(function(){
-//         $('a').hide();
-//     });
-// });
+$(function(){
+  $('.pre-signup-button-web').click(function(){
+    var speed = 2000;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
+
+$(function(){
+  $('.pre-signup-button-ipad').click(function(){
+   var speed = 2000;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
+
+$(function(){
+  $('.pre-signup-button').click(function(){
+    var speed = 2000;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});

acb45e266d6fda8de8422b7dc3425aaad2f2581f
tooma12 <tooma.10ma@gmail.com>
2017-09-23 13:52:27 +0900

    redesign some facts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'tops#index'
-  resources :tops, only: [:index, :new, :create]
+  resources :tops, only: [:index, :new, :create] do
+    collection do
+      get :about
+    end
+  end  
 end

4250ea891a1737a794670af1e670633749bf74e3
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-10-11 17:29:05 +0900

    modify validate

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,6 +1,4 @@
 class Influencer < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
   validates :insta, presence: true
-  validates :age, presence: true
 end

ec90260f036f03ca1a6f8b43a892a87138034387
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-10-13 20:11:45 +0900

    modifyjsfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,27 +17,4 @@
 //= require_tree .
 
 
-$(document).on('turbolinks:load', function() {
-  $(function(){
 
-  //テキストリンクをクリックしたら
- $(".open-button").click(function(){
-   //body内の最後に<div id="modal-bg"></div>を挿入
-   $("body").append('<div class="modal-bg"></div>');
-    //モーダルウィンドウを表示
-    $(".open-button").fadeOut("fast");
-        $(".modal-bg, .menu-bar").fadeIn("fast");
-
-    //画面のどこかをクリックしたらモーダルを閉じる
-      $(".close-button").click(function(){
-        $(".open-button").fadeIn("slow",function(){
-       });
-          $(".menu-bar").fadeOut("fast",function(){
-            //挿入した<div id="modal-bg"></div>を削除
-              $(".modal-bg").remove() ;
-         });
-
-        });
-      });
-    });
-});
---
+++ app/assets/javascripts/open-window.js
@@ -0,0 +1,24 @@
+$(document).on('turbolinks:load', function() {
+  $(function(){
+
+  //テキストリンクをクリックしたら
+ $(".open-button").click(function(){
+   //body内の最後に<div id="modal-bg"></div>を挿入
+   $("body").append('<div class="modal-bg"></div>');
+    //モーダルウィンドウを表示
+    $(".open-button").fadeOut("fast");
+        $(".modal-bg, .menu-bar").fadeIn("fast");
+
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $(".close-button").click(function(){
+        $(".open-button").fadeIn("slow",function(){
+       });
+          $(".menu-bar").fadeOut("fast",function(){
+            //挿入した<div id="modal-bg"></div>を削除
+              $(".modal-bg").remove() ;
+         });
+
+        });
+      });
+    });
+});

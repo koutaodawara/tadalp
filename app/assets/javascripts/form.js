65506af6f345a07c09681d726bb18e4cedc4de81
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-10-13 21:41:16 +0900

    modify application

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,48 +1,48 @@
-// $(document).on('turbolinks:load', function() {
-//   $(function(){
-//   $('.pre-signup-button-web').click(function(){
-//     var speed = 200;
-//     var position = $(this).offset().top;
-//     $("html, body").animate({scrollTop:0}, speed, "swing");
-//     return false;
-//   });
-// });
+$(document).on('turbolinks:load', function() {
+  $(function(){
+  $('.pre-signup-button-web').click(function(){
+    var speed = 200;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
 
-// $(function(){
-//   $('.pre-signup-button-ipad').click(function(){
-//    var speed = 200;
-//     var position = $(this).offset().top;
-//     $("html, body").animate({scrollTop:0}, speed, "swing");
-//     return false;
-//   });
-// });
+$(function(){
+  $('.pre-signup-button-ipad').click(function(){
+   var speed = 200;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
 
-// $(function(){
-//   $('.pre-signup-button').click(function(){
-//     var speed = 200;
-//     var position = $(this).offset().top;
-//     $("html, body").animate({scrollTop:0}, speed, "swing");
-//     return false;
-//   });
-// });
+$(function(){
+  $('.pre-signup-button').click(function(){
+    var speed = 200;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
 
-// $(function(){
-//   $('.signup-button-company').click(function(){
-//     var speed = 200;
-//     var position = $(this).offset().top;
-//     $("html, body").animate({scrollTop:0}, speed, "swing");
-//     return false;
-//   });
-// });
+$(function(){
+  $('.signup-button-company').click(function(){
+    var speed = 200;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
 
-// $(function(){
-//   $('.signup-button-influencer').click(function(){
-//     var speed = 200;
-//     var position = $(this).offset().top;
-//     $("html, body").animate({scrollTop:0}, speed, "swing");
-//     return false;
-//   });
-// });
-// });
+$(function(){
+  $('.signup-button-influencer').click(function(){
+    var speed = 200;
+    var position = $(this).offset().top;
+    $("html, body").animate({scrollTop:0}, speed, "swing");
+    return false;
+  });
+});
+});
 
 

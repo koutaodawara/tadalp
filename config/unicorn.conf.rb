896fdcd7104f83a14891fa067efd0e78f0d4b227
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2017-10-13 20:43:56 +0900

    modify application.css

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,6 +1,6 @@
 $worker  = 2
   $timeout = 30
-  $app_dir = "/home/ユーザー名/アプリ名"
+  $app_dir = "/home/LPtada/tadalp"
   $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
